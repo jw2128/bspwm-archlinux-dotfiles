176f17bc2d366c39177dadb509d597ab01f4eb3a
JC Cabrera Sanchez <64985138+jw2128@users.noreply.github.com>
2022-09-10 09:48:51 -0400

    Delete wal directory

--- wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#b2bab7", "#0c0c0a" },
-	[SchemeSel] = { "#b2bab7", "#5E675A" },
-	[SchemeOut] = { "#b2bab7", "#DAC22C" },
-};
