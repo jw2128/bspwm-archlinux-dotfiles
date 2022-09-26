4f70fb310e50d9a36eb0dade00144a54b4571332
JC Cabrera Sanchez <64985138+jw2128@users.noreply.github.com>
2022-09-25 23:27:30 -0400

    Update temp.sh

--- polybar/script/temp.sh
+++ polybar/script/temp.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-sensors | grep "Package id 0:" | tr -d '+'| awk '{print $4}' | cut -d "." -f 1
+ tempjc=$(sensors | grep "Package id 0:" | tr -d '+'| awk '{print $4}' | cut -d "." -f 1)
+
+echo $tempjc"°C"``
